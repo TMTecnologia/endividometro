0343978366a6361a0eb88d1de3a795bd7ef86715
Thales Menezes <thlmenezes@pm.me>
2022-11-08 00:28:35 -0300

    🔧 syncpack

---
+++ syncpack.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('syncpack/dist/constants').SyncpackConfig} */
+module.exports = {
+  sortFirst: [
+    'name',
+    'version',
+    'description',
+    'repository',
+    'author',
+    'license',
+    'private',
+  ],
+}
